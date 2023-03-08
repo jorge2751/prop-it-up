58b944df4c3a5659802ced6c27e859febffe6f35
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-03-08 14:48:04 -0800

    added birthday button

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 
 export class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    incAge = () => this.setState({age: this.state.age+1})
+
     render() {
-        const {firstName, lastName, age, hairColor} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
         return (
             <div>
                 <h2>{firstName}, {lastName}</h2>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.incAge}>Birthday button for {firstName} {lastName}</button>
             </div>
         )
     }
