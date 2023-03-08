76ef5ce47c6516c5608df66bab622142ffcad82f
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-03-08 14:52:18 -0800

    moved method into render()

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -8,16 +8,15 @@ export class PersonCard extends Component {
         }
     }
 
-    incAge = () => this.setState({age: this.state.age+1})
-
     render() {
         const {firstName, lastName, hairColor} = this.props;
+        const incAge = () => this.setState({age: this.state.age+1})
         return (
             <div>
                 <h2>{firstName}, {lastName}</h2>
                 <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
-                <button onClick={this.incAge}>Birthday button for {firstName} {lastName}</button>
+                <button onClick={incAge}>Birthday button for {firstName} {lastName}</button>
             </div>
         )
     }
